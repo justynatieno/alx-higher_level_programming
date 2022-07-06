f7a8ac609687ae47d3d582c82e90b994ff17b44d
Justyna <justineomwandho@gmail.com>
2022-07-06 18:23:52 +0300

    raises a name

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """raises a name exception with a message
+    Args:
+        message (string): the message to be raised with the name exception
+    """
+    raise NameError(message)
