a1401bc9a7e64fafcb47479536e33eceeb35a5a3
Justyna <justineomwandho@gmail.com>
2022-07-25 18:22:42 +0300

    code fix

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 
 
+"""Defines a class Rectangle."""
+
+
 class Rectangle:
-    """Rectangle class."""
+    """Defines a Rectangle."""
     pass
-
-
-if __name__ == '__main__':
-    my_rectangle = Rectangle()
-    print(type(my_rectangle))
-    print(my_rectangle.__dict__)
