d0d72d17b078096c68473a344c48f6de35f6148f
Justyna <justineomwandho@gmail.com>
2022-08-02 21:58:38 +0300

    fixing indentation error

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,5 +6,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """writes an object to a file using the json representation"""
-    with open(filename, "w", encoding="utf-8) as f:
-    json.dump(my_obj, f)
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
