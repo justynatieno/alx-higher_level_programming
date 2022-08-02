5e74a2264e241708aef312c66bd1c0be9ef6e0a4
Justyna <justineomwandho@gmail.com>
2022-08-02 22:41:04 +0300

    adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 """load, add and save"""
+import os
+import sys
+import json
 
-from sys import argv
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_json = __import__('6-load_from_json_file').load_from_json_file
+    filename = 'add_item.json'
+args = len(sys.argv)
 
-if _name_ == "_main_":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_json = __import__('6-load_from_json_file').load_from_json_file
-    load_from_json_file = load_json
-    filename = "add_item.json"
+if not os.path.isfile(filename):
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write('[]')
 
-    try:
-        list = load_from_json_file(filename)
-    except Exception:
-        list = []
-
-    list.extend(argv[1:])
-    save_to_json_file(list, "add_item.json")
+if args > 1:
+    data = load_from_json_file(filename)
+    for i in range(1, args):
+        data.append(sys.argv[i])
+    save_to_json_file(data, filename)
