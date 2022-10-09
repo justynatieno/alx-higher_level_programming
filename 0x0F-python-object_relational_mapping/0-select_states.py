2ff7bb6f28554f70674d7d645dd7f7bb14a063d8
justyna <justineomwandho@gmail.com>
2022-10-09 19:01:37 +0300

    get all states from databse

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-""" script that lists three states from database hbtn_0e_0_usa"""
-import sys
+
+"""lists all states from the database hbtn_0e_0_usa"""
+
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM 'states ORDER BY states.id ASC'")
-    [print(state) for state in cur.fetchall()]
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
